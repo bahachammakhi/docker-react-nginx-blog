a21feda8bc1feba487750a622855e5538bac56bd
Jared Dyreson <jared.dyreson@gmail.com>
2021-10-28 16:43:04 -0700

    added boiler plate code for the react component Login

--- src/js/components/Login.js
+++ src/js/components/Login.js
@@ -1,3 +1,16 @@
 /*
  * Login Portal
-*/
+ * Example boiler plate React Component, please  base others off this model
+ */
+
+import React from "react";
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.name = "Login";
+    }
+    render() {
+        return <h1>My name is {this.name}</h1>;
+    }
+}
